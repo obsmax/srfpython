277f8925e6f05d76c50bd4f8da4c2aaa9bc879d5
maximilien lehujeur <maximilien.lehujeur@gmail.com>
2020-02-04 17:58:07 +0100

    freeze/restart

--- freeze.sh
+++ freeze.sh
@@ -1,1 +1,5 @@
+#!/bin/bash
+
+# a script to rapidely pause all running threads of HerrMet (using SIGSTOP)
+
 `ps -ef | grep HerrMet | grep -v grep | awk 'BEGIN {printf("kill -SIGSTOP ")} {printf($2" ")} END {print " "}'`
--- restart.sh
+++ restart.sh
@@ -1,1 +1,5 @@
+#!/bin/bash
+
+# a script to restart all paused threads after freeze.sh
+
 `ps -ef | grep HerrMet | grep -v grep | awk 'BEGIN {printf("kill -SIGCONT ")} {printf($2" ")} END {print " "}'`
