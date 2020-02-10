d9d5a769aa4b94503f353a0c4c022ef47632c00b
maximilien lehujeur <maximilien.lehujeur@gmail.com>
2020-02-10 10:55:14 +0100

    update init, HerrMet won't work correctly, needs upgrade

--- srfpython/__init__.py
+++ srfpython/__init__.py
@@ -1,5 +1,5 @@
-from srfpython.Herrmann.Herrmann import dispersion, dispersion_1, dispersion_2, groupbywtm, igroupbywtm, \
-    check_herrmann_codes, recompile_src90
+# from srfpython.Herrmann.Herrmann import dispersion, dispersion_1, dispersion_2, groupbywtm, igroupbywtm, \
+#     check_herrmann_codes, recompile_src90
 from srfpython.depthdisp.depthmodels import depthmodel1D, depthmodel, depthmodel_from_mod96string, \
     depthmodel_from_mod96, depthmodel_from_arrays, depthspace
 from srfpython.depthdisp.dispcurves import Claw, Ulaw, freqspace, \
