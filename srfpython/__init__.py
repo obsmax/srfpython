a55ecaaecee471c3d7cea470d241a842a2521c7c
maximilien lehujeur <maximilien.lehujeur@gmail.com>
2020-04-29 10:38:21 +0200

    useful imports in init

--- srfpython/__init__.py
+++ srfpython/__init__.py
@@ -1,6 +1,6 @@
 from srfpython.Herrmann.Herrmann import check_herrmann_codes, recompile_src90, HerrmannCaller, Curve
 from srfpython.depthdisp.depthmodels import depthmodel1D, depthmodel, depthmodel_from_mod96string, \
-    depthmodel_from_mod96, depthmodel_from_arrays, depthspace
+    depthmodel_from_mod96, depthmodel_from_arrays, depthspace, gardner74, brocher2005
 from srfpython.depthdisp.dispcurves import Claw, Ulaw, freqspace, \
     surf96reader, surf96reader_from_surf96string, surf96reader_from_arrays
 from srfpython.inversion.metropolis2 import metropolis
@@ -9,3 +9,4 @@ from srfpython.standalone.display import plt, gcf, gca, pause, showme, logtick
 from srfpython.standalone.asciifile import AsciiFile
 from srfpython.synthetics.synthetics2 import Green
 import numpy as np
+import sys, glob, os
