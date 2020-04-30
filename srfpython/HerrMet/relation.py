1114813eb094f7a9bf0305e522e9e5188b076ec7
maximilien lehujeur <maximilien.lehujeur@gmail.com>
2020-04-30 15:44:51 +0200

    fnally found how to avoid writing relations to files

--- srfpython/HerrMet/relation.py
+++ srfpython/HerrMet/relation.py
@@ -1,10 +1,24 @@
 class Relation(object):
     # I need a pickable object to pass a function that was defined from a string...
     def __init__(self, name, string):
+        """
+        :param name: name of the relation (i.e. function name)
+        :param string: expression of the function
+            version 1 : string = "lambda x: 3 * x + 2"
+            version 2 : string = "def name(x): return 3 * x + 2"
+        """
         self.name = name
         self.string = string
-        assert self.string.strip().startswith('def {}('.format(name))
-        self.fun = None
+
+        if self.string.strip().startswith('lambda'):
+            self.fun = eval(self.string)
+
+        elif self.string.strip().startswith('def {}('.format(name)):
+            assert "return" in self.string
+            self.fun = eval(compile(self.string, '<string>', 'exec'))
+
+        else:
+            raise ValueError("could not compile {} ".format(string))
 
     def __getstate__(self):
         return self.name, self.string
@@ -14,21 +28,4 @@ class Relation(object):
         Relation.__init__(self, name=name, string=string)
 
     def __call__(self, *args, **kwargs):
-        try:
-            return self.fun(*args, **kwargs)
-        except TypeError as e:
-            if str(e) == "'NoneType' object is not callable":
-
-                import imp
-                modulename = "{}".format(self.name)
-                pyfilename = "./_relation_{}.py".format(modulename)
-                funcname = self.name
-
-                with open(pyfilename, 'w') as fid:
-                    fid.write(self.string + "\n")
-
-                self.fun = getattr(imp.load_source(modulename, pyfilename), funcname)
-                return self.fun(*args, **kwargs)
-
-            else:
-                raise e
+        return self.fun(*args, **kwargs)
