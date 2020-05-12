47267dd6a12bc690a960e41b866f5efb3fa8b0d7
maximilien lehujeur <maximilien.lehujeur@gmail.com>
2020-05-12 22:09:14 +0200

    fuck off bug is back

--- tutorials/02_cube_inversion_example/200_optimize.sh
+++ tutorials/02_cube_inversion_example/200_optimize.sh
@@ -1,10 +1,22 @@
 mkdir optimize
 cd optimize || exit 1
 
+## works
+#HerrMet --optimize -init ../nodes.txt \
+#  -data 1. 0.0 \
+#  -prior  100. 1. \
+#          200. 2. \
+#          1 \
+#          1.0 0.5 \
+#  -upd 3 0 \
+#  -save
+
+
+# do not work
 HerrMet --optimize -init ../nodes.txt \
   -data 1. 0.0 \
   -prior  100. 1. \
-          200. 2. \
+          100. 1. \
           1 \
           1.0 0.5 \
   -upd 3 0 \
