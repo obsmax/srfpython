e3fb71c6ea701d04f1476ed4725f018503f26bc7
maximilien lehujeur <maximilien.lehujeur@gmail.com>
2020-05-19 16:16:26 +0200

    rerun test

--- tutorials/02_cube_inversion_example/150_extract.sh
+++ tutorials/02_cube_inversion_example/150_extract.sh
@@ -2,7 +2,7 @@
 cd inversion || exit 1;
 
 # remove previous extracted files if any
-find inversion -name "_HerrMet.best_*.p0.??.*96" -delete
+# find inversion -name "_HerrMet.best_*.p0.??.*96" -delete
 
 HerrMet --extract  \
     -pdf best 1000 0 1
