c8da5851f9aef0cd91fac9eb3051edb3e371a4f6
mlehujeur <maximilien.lehujeur@gmail.com>
2022-10-05 21:34:24 +0200

    minor update in test suite

--- tutorials/01_simple_inversion_example/150_extract.sh
+++ tutorials/01_simple_inversion_example/150_extract.sh
@@ -6,8 +6,8 @@ rm -f ./_HerrMet_data010/*best_*mod96
 # extract the 10 best models and 16% 50% 84% percentiles of the 10^4 best models
 HerrMet --extract _HerrMet_data010 \
     -top 10 0 1 \
-    -pdf best 10000 0 1
+    -pdf best 1000 0 1
 
 m96 --show \
     _HerrMet_data010/*rank*.mod96 \
-    _HerrMet_data010/_HerrMet.best_10000_0_1.p*.mod96
+    _HerrMet_data010/_HerrMet.best_1000_0.00_1.p*.mod96
