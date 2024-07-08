40f429659039f81fc94c640f14c5dab362e68a66
maximilien.lehujeur <maximilien.lehujeur@univ-eiffel.fr>
2024-07-08 21:08:41 +0200

    simplify main

--- tutorials/02_acoustic_scale_inversion_example/main_script.sh
+++ tutorials/02_acoustic_scale_inversion_example/main_script.sh
@@ -9,20 +9,20 @@ echo "HerrMet --target picks.surf96"
 HerrMet --target picks.surf96
 
 echo "# =========== Prepare 3 different inversions"
-cp -av _HerrMet_picks _HerrMet_2layers
-cp -av _HerrMet_picks _HerrMet_3layers
-mv _HerrMet_picks _HerrMet_4layers
+# cp -av _HerrMet_picks _HerrMet_4layers
+# cp -av _HerrMet_picks _HerrMet_3layers
+mv _HerrMet_picks _HerrMet_2layers
 
 echo "# =========== Prepare 3 different parameterizations for comparison"
 # Note : in most cases, you must edit the parameterization files before inversion
 HerrMet --param 2 0.00004 -growing -t mZVSPRRH -op
 HerrMet --send _HerrMet_2layers
 
-HerrMet --param 3 0.00004 -growing -t mZVSPRRH -op
-HerrMet --send _HerrMet_3layers
+# HerrMet --param 3 0.00004 -growing -t mZVSPRRH -op
+# HerrMet --send _HerrMet_3layers
 
-HerrMet --param 4 0.00004 -growing -t mZVSPRRH -op
-HerrMet --send _HerrMet_4layers
+# HerrMet --param 4 0.00004 -growing -t mZVSPRRH -op
+# HerrMet --send _HerrMet_4layers
 
 echo "# =========== Check the target and prior boundaries"
 echo "HerrMet --display"
@@ -34,11 +34,11 @@ HerrMet -w 12 --run -mode restart -nchain 4 -nkeep 1000
 
 echo "# =========== Display results"
 echo "HerrMet --HerrMet --display"
+# -compact -si \
 HerrMet --display \
     -plot best 500 0 1 \
     -pdf best 500 0 1 \
     -overdisp \
-    -compact -si \
     -png
 eog ./_HerrMet*/*png
 
